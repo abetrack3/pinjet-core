16c52b8ad31372ddd6521ef4e4d600549f242b32
Abrar <abetrack3@yahoo.com>
2024-04-09 18:11:53 +0600

    added setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+
+VERSION = '0.0.1-beta.1'
+DESCRIPTION = 'Small like a pin, fast like a jet! A lightweight Injection library for python'
+AUTHOR = 'abetrack3 (Abrar Shahriar Abeed)'
+AUTHOR_EMAIL = '<abrarshahriar2361@gmail.com>'
+
+setup(
+    name='pinjet-core',
+    version=VERSION,
+    description=DESCRIPTION,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    install_requires=[
+        'pinjet-common~=1.0.2'
+    ],
+)
