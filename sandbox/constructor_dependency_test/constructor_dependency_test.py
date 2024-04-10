26efb3a59d399a062ea2e851be565639e34e5bd4
Abrar <abetrack3@yahoo.com>
2024-04-10 19:31:08 +0600

    rewrite test case using unittest: constructor_dependency_test.py

--- sandbox/constructor_dependency_test/constructor_dependency_test.py
+++ sandbox/constructor_dependency_test/constructor_dependency_test.py
@@ -1,5 +1,6 @@
-from pinjet.core.resolver.dependency_resolver import DependencyResolver
+import unittest
 from pinjet.core.annotations.injectable import injectable
+from pinjet.core.resolver.dependency_resolver import DependencyResolver
 
 
 @injectable
@@ -22,15 +23,28 @@ class Class3:
 @injectable
 class Class2:
 
-    def __init__(self, a: Class3, b: Class3):
-        self.a = a
-        self.b = b
+    def __init__(self, a: Class1, b: Class3):
+        self.a: Class1 = a
+        self.b: Class3 = b
 
     def __str__(self) -> str:
         return f'Class2->["self.a":"{self.a}", "self.b":"{self.b}"]'
 
 
-resolved_instance: Class2 = DependencyResolver.resolve(Class2)
+class TestConstructorDependency(unittest.TestCase):
 
-print(resolved_instance)
+    def test_constructor_dependency(self):
 
+        # Act
+        resolved_instance: Class2 = DependencyResolver.resolve(Class2)
+
+        # Assert
+        self.assertIsInstance(resolved_instance, Class2)
+        self.assertIsInstance(resolved_instance.a, Class1)
+        self.assertIsInstance(resolved_instance.b, Class3)
+        self.assertIsInstance(resolved_instance.b.a, Class1)
+
+
+if __name__ == '__main__':
+    print('Constructor Dependency Test')
+    unittest.main()
