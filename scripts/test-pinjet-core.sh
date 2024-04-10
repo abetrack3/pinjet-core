44dddf0cbc990e4c2b73481a20c3c00f35e12f6a
Abrar <abetrack3@yahoo.com>
2024-04-10 19:34:53 +0600

    added script to run all tests with a single command

---
+++ scripts/test-pinjet-core.sh
@@ -0,0 +1,8 @@
+python ../sandbox/basic_resolver_test/basic_resolver_test.py
+python ../sandbox/circular_dependency_test/circular_dependency_test.py
+python ../sandbox/constructor_dependency_test/constructor_dependency_test.py
+python ../sandbox/provider_test/provider_test.py
+python ../sandbox/resource_scanner_test/resource_scanner_test.py
+python ../sandbox/scope_tests/basic_prototype_test/basic_prototype_test.py
+python ../sandbox/scope_tests/basic_singleton_test/basic_singleton_test.py
+python ../sandbox/scope_tests/thread_safe_singleton_test/thread_safe_singleton_test.py
