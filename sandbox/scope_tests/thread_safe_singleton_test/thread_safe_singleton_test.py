7f060a2287dfc3228428a761ab949b281279c116
Abrar <abetrack3@yahoo.com>
2024-04-10 19:34:20 +0600

    rewrite test case using unittest: thread_safe_singleton_test.py

--- sandbox/scope_tests/thread_safe_singleton_test/thread_safe_singleton_test.py
+++ sandbox/scope_tests/thread_safe_singleton_test/thread_safe_singleton_test.py
@@ -1,8 +1,8 @@
-import concurrent.futures
 import time
 import unittest
-from pinjet.core.resolver.dependency_resolver import DependencyResolver
+import concurrent.futures
 from pinjet.core.annotations.injectable import injectable
+from pinjet.core.resolver.dependency_resolver import DependencyResolver
 
 
 @injectable
@@ -14,26 +14,26 @@ class TestSingletonResolverThreadSafety(unittest.TestCase):
 
     def test_singleton_thread_safety(self):
 
-        def resolve_singleton(worker_id: int):
-
-            print(f'worker {worker_id}')
+        # Arrange
+        def resolve_singleton():
 
             resolved_instance = DependencyResolver.resolve(ClassToBeResolved)
-
-            print(f'resolved instance: {resolved_instance} by worker {worker_id}')
 
             time.sleep(0.1)
 
             return resolved_instance
 
+        # Act
         with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
 
-            futures = [executor.submit(resolve_singleton, _) for _ in range(10)]
+            futures = [executor.submit(resolve_singleton) for _ in range(10)]
 
             instances = [future.result() for future in concurrent.futures.as_completed(futures)]
 
+        # Assert
         self.assertEqual(len(set(instances)), 1)
 
 
 if __name__ == '__main__':
+    print('Thread Safe Singleton Test')
     unittest.main()
