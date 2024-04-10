7cbebe63b80c3ef71de5d165f2c784ab7a7765b8
Abrar <abetrack3@yahoo.com>
2024-04-10 19:30:42 +0600

    rewrite test case using unittest: circular_dependency_test.py

--- sandbox/circular_dependency_test/circular_dependency_test.py
+++ sandbox/circular_dependency_test/circular_dependency_test.py
@@ -1,5 +1,7 @@
+import unittest
 from pinjet.core.annotations.injectable import injectable
 from pinjet.core.resolver.dependency_resolver import DependencyResolver
+from pinjet.core.exception.exceptions import CircularDependencyException
 
 
 @injectable
@@ -17,6 +19,15 @@ class Class3:
     pass
 
 
+def init_0(self):
+    """
+    Empty/Default Constructor with no dependencies
+    :param self:
+    :return:
+    """
+    pass
+
+
 def init_1(self, a: Class2):
     self.a = a
 
@@ -29,10 +40,33 @@ def init_3(self, c: Class1):
     self.c = c
 
 
-Class1.__init__ = init_1
-Class2.__init__ = init_2
-Class3.__init__ = init_3
+class TestCircularDependency(unittest.TestCase):
+
+    def test_resolve_no_circular_dependency(self):
+
+        # Arrange
+        Class1.__init__ = init_1
+        Class2.__init__ = init_2
+        Class3.__init__ = init_0
+
+        # Act
+        resolved_instance = DependencyResolver.resolve(Class1)
+
+        # Assert
+        self.assertIsInstance(resolved_instance, Class1)
+
+    def test_resolve_circular_dependency(self):
+
+        # Arrange
+        Class1.__init__ = init_1
+        Class2.__init__ = init_2
+        Class3.__init__ = init_3
+
+        # Act & Assert
+        with self.assertRaises(CircularDependencyException):
+            DependencyResolver.resolve(Class1)
 
 
-resolved_instance = DependencyResolver.resolve(Class1)
-
+if __name__ == '__main__':
+    print('Circular Dependency Test')
+    unittest.main()
