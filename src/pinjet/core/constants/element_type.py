1abdacf5af10a87d5fcc30ecfe19638edcf8b3f4
Abrar <abetrack3@yahoo.com>
2024-04-12 17:37:49 +0600

    removed generic method from element type, added more specifics and undetermined mode

--- src/pinjet/core/constants/element_type.py
+++ src/pinjet/core/constants/element_type.py
@@ -3,5 +3,8 @@ from strenum import StrEnum
 
 class ElementType(StrEnum):
     CLASS = 'class'
-    METHOD = 'method'
     FUNCTION = 'function'
+    STATIC_METHOD = FUNCTION  # haven't found any way to programmatically distinguish these two
+    INSTANCE_METHOD = 'instance_method'
+    CLASS_METHOD = 'class_method'
+    UNDETERMINED = 'undetermined'
