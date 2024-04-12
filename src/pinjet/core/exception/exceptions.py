99069205b7c4c8b4ada198fc1bc267a7f6143771
Abrar <abetrack3@yahoo.com>
2024-04-12 17:38:49 +0600

    added more verbosity in exception message for type mismatch exception

--- src/pinjet/core/exception/exceptions.py
+++ src/pinjet/core/exception/exceptions.py
@@ -1,3 +1,9 @@
+from typing import Callable
+
+from pinjet.common.constants import String
+from pinjet.core.constants.element_type import ElementType
+
+
 class UnregisteredDependencyException(Exception):
     pass
 
@@ -22,5 +28,15 @@ class TargetTypeNotSpecifiedException(Exception):
     pass
 
 
-class AnnotationTargetTypeMismatchException(Exception):
-    pass
+class TargetElementTypeMisMatchException(Exception):
+
+    def __init__(self,
+                 target_decorator: Callable,
+                 declared_element_type: ElementType,
+                 received_element_type: ElementType):
+
+        super().__init__(
+            f'Target decorator: "{target_decorator.__qualname__}" placed wrongfully.{String.NEXT_LINE}'
+            f'Should be applied to of type: "{declared_element_type.upper()}" only.{String.NEXT_LINE}'
+            f'But found to be applied to of type: "{received_element_type.upper()}".{String.NEXT_LINE}'
+        )
