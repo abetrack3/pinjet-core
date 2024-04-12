696abd978bcb0f2e03ba7d2a44985f87d6a5027f
Abrar <abetrack3@yahoo.com>
2024-04-12 17:40:47 +0600

    revamped the core logic behind target type enforcement including given callable object's target type detection

--- src/pinjet/core/annotations/target.py
+++ src/pinjet/core/annotations/target.py
@@ -1,41 +1,101 @@
 import inspect
-from typing import Callable, Optional
-from functools import wraps
+import functools
+from typing import List, Callable
 
+from pinjet.common.constants import Index, Integer
+
+from ..exception.exceptions import TargetElementTypeMisMatchException, TargetTypeNotSpecifiedException
 from ..constants.element_type import ElementType
-from ..exception.exceptions import TargetTypeNotSpecifiedException, AnnotationTargetTypeMismatchException
 
 
-def target(element: ElementType) -> Callable:
+def target(declared_target_element_type):
 
-    if element is None:
+    if declared_target_element_type is None or not isinstance(declared_target_element_type, ElementType):
+
         raise TargetTypeNotSpecifiedException
 
-    if not isinstance(element, ElementType):
-        raise ValueError(f'Target element {element} is not an instance of AnnotationTargetType')
+    elif (isinstance(declared_target_element_type, ElementType)
+          and declared_target_element_type == ElementType.UNDETERMINED):
 
-    def wrapper(target_decorator_function: Callable) -> Callable:
+        raise ValueError('Target type cannot be undetermined')
 
-        @wraps(target_decorator_function)
-        def decorated(callable_object: Optional[Callable] = None, *args, **kwargs) -> Callable:
+    def decorator(target_decorator):
 
-            if callable_object is None:
-                return target_decorator_function(*args, **kwargs)
+        @functools.wraps(target_decorator)
+        def wrapper(*args, **kwargs):
 
-            if element is ElementType.FUNCTION and inspect.isfunction(callable_object) is not True:
-                raise AnnotationTargetTypeMismatchException
+            # Check if the target_decorator accepts its own arguments
+            if args and callable(args[Index.ZERO]):
 
-            if element is ElementType.CLASS and inspect.isclass(callable_object) is not True:
-                raise AnnotationTargetTypeMismatchException
+                # If args[0] is a callable, it's likely a function or method
+                element_type = __detect_element_type(args[Index.ZERO])
 
-            if (element is ElementType.METHOD and
-                    (inspect.isfunction(callable_object) or
-                     inspect.ismethod(callable_object)) is not True):
+                if element_type != declared_target_element_type:
+                    raise TargetElementTypeMisMatchException(target_decorator,
+                                                             declared_target_element_type,
+                                                             element_type)
 
-                raise AnnotationTargetTypeMismatchException
+            else:
 
-            return target_decorator_function(callable_object, *args, **kwargs)
+                # Check if the target_decorator is applied to an element
+                if len(args) > 1:
 
-        return decorated
+                    element_type = __detect_element_type(args[Index.ONE])
 
-    return wrapper
+                    if element_type != declared_target_element_type:
+                        raise TargetElementTypeMisMatchException(target_decorator,
+                                                                 declared_target_element_type,
+                                                                 element_type)
+
+            # Call the target_decorator
+            return target_decorator(*args, **kwargs)
+
+        return wrapper
+
+    return decorator
+
+
+def __is_class_method(element: Callable, args: List[str]) -> bool:
+
+    if isinstance(element, classmethod):
+        return True
+
+    if len(args) != Integer.ZERO and args[Index.ZERO] == 'cls':
+        return True
+
+    return False
+
+
+def __is_instance_method(args: List[str]) -> bool:
+
+    if len(args) != Integer.ZERO and args[Index.ZERO] == 'self':
+        return True
+
+    return False
+
+
+def __detect_element_type(element) -> ElementType:
+
+    if isinstance(element, type):
+
+        return ElementType.CLASS
+
+    elif hasattr(element, '__call__'):
+
+        element_arguments = inspect.getfullargspec(element).args
+
+        if __is_class_method(element, element_arguments):
+
+            return ElementType.CLASS_METHOD
+
+        elif __is_instance_method(element_arguments):
+
+            return ElementType.INSTANCE_METHOD
+
+        elif inspect.isfunction(element):
+
+            return ElementType.FUNCTION
+
+        else:
+
+            return ElementType.UNDETERMINED
