c30734e8ef38902a23c906a9247c26886aea39a4
Abrar <abetrack3@yahoo.com>
2024-04-12 17:05:25 +0600

    minor code improvement

--- src/pinjet/core/annotations/bootstrap.py
+++ src/pinjet/core/annotations/bootstrap.py
@@ -1,3 +1,4 @@
+from functools import wraps
 from typing import Callable, Any
 
 from .target import target
@@ -8,6 +9,7 @@ from ..resource_scanner.resource_scanner import RepositoryScanner
 @target(ElementType.FUNCTION)
 def pinjet_application(function: Callable) -> Callable:
 
+    @wraps(function)
     def decorated_function(*args, **kwargs) -> Any:
 
         RepositoryScanner.scan_repository(function)
